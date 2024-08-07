59613470cd2a20580ccf152eb08344f862c45c91
Abel <111386807+abel325@users.noreply.github.com>
2024-08-07 15:44:57 +0300

    initial commit

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    // use HasFactory;
+    use HasTimestamps;
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (!User::where('email', 'admin@practica.local')->exists()) {
+            $user = new User();
+            $user->name = 'Admin';
+            $user->email = 'admin@practica.local';
+            $user->password = Hash::make('parola');
+            $user->save();
+        } 
+    }
+}
