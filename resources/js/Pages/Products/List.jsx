a91330f5786828b96b51cb853544e829585103be
Abel <111386807+abel325@users.noreply.github.com>
2024-08-16 19:39:39 +0300

    remove console log

--- resources/js/Pages/Products/List.jsx
+++ resources/js/Pages/Products/List.jsx
@@ -19,8 +19,6 @@ export default function List({ auth, products }) {
         }
     };
 
-    console.log(products);
-
     return (
         <AuthenticatedLayout user={auth.user}>
             <Head title="Product List" />
