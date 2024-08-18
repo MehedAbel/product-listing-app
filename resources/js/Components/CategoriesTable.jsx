034a86db301a93aa98d8d35c0540b949ca0cc6fd
Abel <111386807+abel325@users.noreply.github.com>
2024-08-18 13:21:52 +0300

    change categories list

---
+++ resources/js/Components/CategoriesTable.jsx
@@ -0,0 +1,76 @@
+import { Link, router } from "@inertiajs/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+export default function CategoriesTable({ categories }) {
+    const handleDelete = (id) => {
+        if (confirm("Are you sure you want to delete this category?")) {
+            router.delete(route("categories.delete", id));
+        }
+    };
+
+    return (
+        <table className="w-full">
+            <thead>
+                <tr className="">
+                    <th className="font-bold p-2">
+                        <h2>ID</h2>
+                    </th>
+                    <th className="font-bold p-2">
+                        <h2>Name</h2>
+                    </th>
+                    <th className="font-bold p-2">
+                        <h2>Order</h2>
+                    </th>
+                    <th className="font-bold p-2">
+                        <h2>Actions</h2>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {categories.map((category, index) => {
+                    return (
+                        <tr
+                            key={index}
+                            className="odd:bg-white even:bg-gray-100"
+                        >
+                            <td className="py-3 px-4 text-center">
+                                <div className="">{category.id}</div>
+                            </td>
+                            <td className="py-2 px-4">
+                                <div className="">{category.name}</div>
+                            </td>
+                            <td className="py-2 px-4 text-center">
+                                <div className="">{category.order}</div>
+                            </td>
+                            <td className="py-2 px-4">
+                                <div className="flex gap-6 justify-center items-center">
+                                    <Link
+                                        href={route("categories.update", [
+                                            category.id,
+                                        ])}
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faPencil}
+                                            className="cursor-pointer hover:text-blue-500 hover:scale-125"
+                                        />
+                                    </Link>
+                                    <button
+                                        onClick={() =>
+                                            handleDelete(category.id)
+                                        }
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faTrash}
+                                            className="cursor-pointer hover:text-red-500 hover:scale-125"
+                                        />
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+}
--- resources/js/Pages/Categories/List.jsx
+++ resources/js/Pages/Categories/List.jsx
@@ -1,17 +1,9 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import ContentLayout from "@/Layouts/ContentLayout";
-import { Head, Link, router } from "@inertiajs/react";
-import { Fragment } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Head, Link } from "@inertiajs/react";
+import CategoriesTable from "@/Components/CategoriesTable";
 
 export default function List({ auth, categories }) {
-    const handleDelete = (id) => {
-        if (confirm("Are you sure you want to delete this category?")) {
-            router.delete(route("categories.delete", id));
-        }
-    };
-
     return (
         <AuthenticatedLayout user={auth.user}>
             <Head title="Categories" />
@@ -28,67 +20,7 @@ export default function List({ auth, categories }) {
                 </div>
 
                 <div className="mt-6">
-                    <div className="grid grid-cols-4">
-                        <div className="w-1/4">
-                            <h2 className="font-bold mb-9">ID</h2>
-                        </div>
-                        <div className="w-1/4">
-                            <h2 className="font-bold mb-9">Name</h2>
-                        </div>
-                        <div className="w-1/4">
-                            <h2 className="font-bold mb-9">Order</h2>{" "}
-                        </div>
-                        <div className="w-1/4">
-                            <h2 className="font-bold mb-9">Actions</h2>
-                        </div>
-
-                        {categories.map((category, index) => {
-                            return (
-                                <Fragment key={index}>
-                                    <div className="w-1/4 flex items-center">
-                                        <div className=" mb-2">
-                                            {category.id}
-                                        </div>
-                                    </div>
-                                    <div className="w-1/4 flex items-center text-nowrap">
-                                        <div className="mb-2 ">
-                                            {category.name}
-                                        </div>
-                                    </div>
-                                    <div className="w-1/4 flex items-center">
-                                        <div className=" mb-2">
-                                            {category.order}
-                                        </div>
-                                    </div>
-                                    <div className="w-1/4 flex items-center">
-                                        <div className="mb-2 flex gap-6">
-                                            <Link
-                                                href={route(
-                                                    "categories.update",
-                                                    [category.id]
-                                                )}
-                                            >
-                                                <FontAwesomeIcon
-                                                    icon={faPencil}
-                                                    className="cursor-pointer hover:text-blue-500 hover:scale-125"
-                                                />
-                                            </Link>
-                                            <button
-                                                onClick={() =>
-                                                    handleDelete(category.id)
-                                                }
-                                            >
-                                                <FontAwesomeIcon
-                                                    icon={faTrash}
-                                                    className="cursor-pointer hover:text-red-500 hover:scale-125"
-                                                />
-                                            </button>
-                                        </div>
-                                    </div>
-                                </Fragment>
-                            );
-                        })}
-                    </div>
+                    <CategoriesTable categories={categories} />
                 </div>
             </ContentLayout>
         </AuthenticatedLayout>
