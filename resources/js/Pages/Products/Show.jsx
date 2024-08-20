1ad3ffd5ee6f5b8511486cece89a0d1d55d26b04
Abel <111386807+abel325@users.noreply.github.com>
2024-08-20 21:13:12 +0300

    modify show product

--- resources/js/Pages/Products/Show.jsx
+++ resources/js/Pages/Products/Show.jsx
@@ -12,16 +12,16 @@ export default function Show({ auth, product, category, images }) {
                 <h1 className="font-bold text-2xl text-center">Show Product</h1>
                 <div className="mt-3 flex flex-col items-center justify-center gap-5">
                     <ImageGallery images={images} />
-                    <div className="flex justify-between w-full flex-col sm:flex-row">
-                        <h2 className="font-semibold text-xl">
-                            {product.name}
-                        </h2>
-                        <div className="font-bold text-2xl  ">
+                    <div className="flex justify-between w-full flex-col">
+                        <h2 className=" text-4xl">{product.name}</h2>
+                        <div className="font-bold text-4xl mt-5">
                             {"€" + product.price}
                         </div>
                     </div>
-                    <div className="flex justify-start w-full text-lg">
-                        {product.description}
+                    <div className="bg-zinc-700 h-px w-full"></div>
+                    <div className="flex justify-start w-full text-lg flex-col gap-5">
+                        <h2 className="font-semibold text-2xl">DESCRIPTION</h2>
+                        <div>{product.description}</div>
                     </div>
                     <div className="flex justify-start w-full text-md text-blue-600">
                         {category ? "#" + category.name : "#no category"}
